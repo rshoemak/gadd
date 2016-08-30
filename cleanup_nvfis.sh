fd80d68d59d1df6bbdec48433e71dc03164f2bff
cpuskarz <cpuskarz@cisco.com>
2016-08-29 20:18:26 -0500

    added content-type header to cleanup script with PUT command 	modified:   cleanup_nvfis.sh

--- cleanup_nvfis.sh
+++ cleanup_nvfis.sh
@@ -3,16 +3,22 @@
 file="GADD-ASA_input_cfg.json"
 echo
 echo "Removing deployed ASAv"
-curl -k -v -u $NFVIS_LOGIN_INPUT:$NFVIS_PASSWRD_INPUT -X DELETE $NFVIS_URL_INPUT/api/config/esc_datamodel/tenants/tenant/admin/deployments/deployment/ASAv
+curl -k -v -u $NFVIS_LOGIN_INPUT:$NFVIS_PASSWRD_INPUT -X DELETE \
+$NFVIS_URL_INPUT/api/config/esc_datamodel/tenants/tenant/admin/deployments/deployment/ASAv
 echo
 echo "Moving ISRv back to LAN Network"
-curl -k -v -u $NFVIS_LOGIN_INPUT:$NFVIS_PASSWRD_INPUT -X PUT $NFVIS_URL_INPUT/api/config/esc_datamodel/tenants/tenant/admin/deployments/deployment/1471415647/vm_group/ROUTER/interfaces -d @network_map.json
+curl -k -v -u $NFVIS_LOGIN_INPUT:$NFVIS_PASSWRD_INPUT -X PUT \
+$NFVIS_URL_INPUT/api/config/esc_datamodel/tenants/tenant/admin/deployments/deployment/1471415647/vm_group/ROUTER/interfaces \
+-H "Content-type: application/vnd.yang.data+json" \
+-d @network_map.json
 echo
 echo "Removing network"
-curl -k -v -u $NFVIS_LOGIN_INPUT:$NFVIS_PASSWRD_INPUT -X DELETE $NFVIS_URL_INPUT/api/config/networks/network/svc-gadd-net
+curl -k -v -u $NFVIS_LOGIN_INPUT:$NFVIS_PASSWRD_INPUT -X \
+DELETE $NFVIS_URL_INPUT/api/config/networks/network/svc-gadd-net
 echo
 echo "Removing bridge"
-curl -k -v -u $NFVIS_LOGIN_INPUT:$NFVIS_PASSWRD_INPUT -X DELETE $NFVIS_URL_INPUT/api/config/bridges/bridge/svc-gadd-br
+curl -k -v -u $NFVIS_LOGIN_INPUT:$NFVIS_PASSWRD_INPUT -X \
+DELETE $NFVIS_URL_INPUT/api/config/bridges/bridge/svc-gadd-br
 echo
 echo "Remove input json file"
 if [ -f $file ] ; then
