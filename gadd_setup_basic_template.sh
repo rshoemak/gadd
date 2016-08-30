477b9c62cfc2967c4e76fb821d9ac48ce7e90192
cpuskarz <cpuskarz@cisco.com>
2016-08-30 15:56:20 -0500

    README stuff.

--- gadd_setup_basic_template.sh
+++ gadd_setup_basic_template.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # copy this file to "gadd_setup_basic.sh"
 # edit the file per README.md instructions
 # run this script from the command: "source gadd_setup_basic.sh"
@@ -5,10 +6,10 @@
 
 # Static environmental variables.
 export TOKEN_INPUT="Bearer XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-export APIC_URL_INPUT='http://x.x.x.x'
-export APIC_LOGIN_INPUT='login_name'
-export APIC_PASSWRD_INPUT='password'
-export NFVIS_LOGIN_INPUT='login_name'
-export NFVIS_PASSWRD_INPUT='password'
-export NFVIS_URL_INPUT='https://x.x.x.x'
-export TRIGGER_EVENT_IP='10.100.10.10'
+export APIC_URL_INPUT='http://10.91.86.180'
+export APIC_LOGIN_INPUT='devopsone'
+export APIC_PASSWRD_INPUT='devopsoone'
+export NFVIS_LOGIN_INPUT='admin'
+export NFVIS_PASSWRD_INPUT='admin'
+export NFVIS_URL_INPUT='https://10.91.13.154'
+export TRIGGER_EVENT_IP='10.100.1.10'
